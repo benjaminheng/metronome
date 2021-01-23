968f3acf0053a462bc9225ff233328f5c9f5a9c0
Benjamin Heng <benjamin.heng@thecarousell.com>
2021-01-23 18:41:04 +0800

    Refactor to use a class

--- metronome.js
+++ metronome.js
@@ -1,62 +1,110 @@
-function playBeat(audioCtx, destinationNode, buffer) {
-  let source = audioCtx.createBufferSource();
-  source.connect(destinationNode);
-  source.buffer = buffer;
-  source.start()
+class ScheduledNote {
+  constructor(audioNode, time) {
+    this.node = audioNode;
+    this.time = time;
+  }
 }
 
-function setVolume(audioCtx, gainNode, volumePercent) {
-  const maxVolume = 0.2;
-  const volume = volumePercent / 100
-  gainNode.gain.setValueAtTime(maxVolume*volume, audioCtx.currentTime)
-  let volumeText = document.getElementById("volumeText");
-  volumeText.innerHTML = volumePercent.toString() + "%";
+class Metronome {
+  constructor() {
+    this.audioCtx = new AudioContext();
+    this.gainNode = this.audioCtx.createGain();
+    this.gainNode.connect(this.audioCtx.destination);
+
+    this.scheduleFrequencyMS = 25;
+    this.maxScheduleAheadTimeSeconds = 0.1;
+    this.scheduledNotes = [];
+    this.lastScheduledNoteTimeSeconds = 0;
+
+    this.metronomeInterval = null;
+
+    this.bpm = 60;
+    this.volumeScale = 1.0;
+  }
+
+  scheduler() {
+    if (this.lastScheduledNoteTimeSeconds < this.audioCtx.currentTime + this.maxScheduleAheadTimeSeconds) {
+      const base = Math.max(this.lastScheduledNoteTimeSeconds, this.audioCtx.currentTime);
+      const secondsPerBeat = 60/this.bpm;
+      const nextTime = base + secondsPerBeat;
+      this.scheduleNote(base + secondsPerBeat);
+      this.lastScheduledNoteTimeSeconds = nextTime;
+    }
+  }
+
+  start() {
+    this.stop();
+    this.metronomeInterval = setInterval(this.scheduler.bind(this), this.scheduleFrequencyMS);
+  }
+
+  stop() {
+    this.removeScheduledNotes();
+    if (this.metronomeInterval !== null) {
+      clearInterval(this.metronomeInterval);
+      this.metronomeInterval = null;
+    }
+  }
+
+  isRunning() {
+    return this.metronomeInterval !== null;
+  }
+
+  scheduleNote(time) {
+    const oscillator = this.audioCtx.createOscillator();
+    oscillator.frequency.value = 440;
+    oscillator.connect(this.gainNode);
+    oscillator.start(time);
+    oscillator.stop(time + 0.1);
+    this.scheduledNotes.push(new ScheduledNote(oscillator, time));
+  }
+
+  setVolume(volumePercent) {
+    const volume = volumePercent / 100
+    this.gainNode.gain.setValueAtTime(this.volumeScale*volume, this.audioCtx.currentTime)
+  }
+
+  removeScheduledNotes() {
+    for (let i=0; i<this.scheduledNotes.length; i++) {
+      try {
+        this.scheduledNotes[i].node.disconnect(this.gainNode);
+      } catch {}
+    }
+  }
+
+  setBPM(bpm) {
+    this.removeScheduledNotes();
+    this.bpm = bpm;
+    this.lastScheduledNoteTimeSeconds = 0;
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   const volumeSlider = document.getElementById("volumeSlider");
-  const startBtn = document.getElementById("startBtn");
   const bpmInput = document.getElementById("bpmInput");
+  const startBtn = document.getElementById("startBtn");
 
-  let audioCtx = new AudioContext();
-  let gain = audioCtx.createGain();
-  setVolume(audioCtx, gain, volumeSlider.valueAsNumber);
-  gain.connect(audioCtx.destination);
-
-  let beatLength = 0.1; // 100ms
-  let buffer = audioCtx.createBuffer(1, beatLength*audioCtx.sampleRate, audioCtx.sampleRate);
-
-  // fill buffer with white noise
-  let bufferData = buffer.getChannelData(0);
-  for (let i=0; i<buffer.length; i++) {
-    bufferData[i] = Math.random() * 2 - 1; // [-1.0; 1.0]
-  }
-
-  let metronomeInterval = null;
+  const metronome = new Metronome();
+  metronome.setVolume(volumeSlider.valueAsNumber)
+  metronome.setBPM(bpmInput.valueAsNumber);
 
   startBtn.addEventListener("click", function(e){
-    if (metronomeInterval === null) {
-      playBeat(audioCtx, gain, buffer);
-      metronomeInterval = setInterval(function(){
-        playBeat(audioCtx, gain, buffer);
-      }, 60000/bpmInput.valueAsNumber);
+    if (metronome.isRunning()) {
+      metronome.stop();
     } else {
-      clearInterval(metronomeInterval);
-      metronomeInterval = null;
+      metronome.start();
     }
-    e.target.innerHTML = metronomeInterval === null ? "Start" : "Stop";
+    e.target.innerHTML = metronome.isRunning() ? "Stop" : "Start";
   });
 
   volumeSlider.addEventListener("input", function(e){
-    setVolume(audioCtx, gain, e.target.valueAsNumber)
+    metronome.setVolume(e.target.valueAsNumber)
+    const volumeText = document.getElementById("volumeText");
+    volumeText.innerHTML = e.target.value + "%";
   });
 
   bpmInput.addEventListener("input", function(e){
-    if (metronomeInterval !== null) {
-      clearInterval(metronomeInterval);
-      metronomeInterval = setInterval(function(){
-        playBeat(audioCtx, gain, buffer);
-      }, 60000/e.target.valueAsNumber);
+    if (e.target.valueAsNumber >= e.target.min && e.target.valueAsNumber <= e.target.max) {
+      metronome.setBPM(e.target.valueAsNumber);
     }
   });
 });
